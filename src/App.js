33ba9e8f4344c1c2aa709f9fd50be92692b052be
FAE01470 <buzzcatfish@gmail.com>
2021-12-27 20:35:23 +0900

    수정 기능

--- src/App.js
+++ src/App.js
@@ -1,75 +1,85 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import CreateTask from './components/CreateTask';
-import Tasks from './components/Tasks';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import CreateTask from "./components/CreateTask";
+import Tasks from "./components/Tasks";
+import "./App.css";
 
 const Header = styled.header`
-    font-size: 24px;
-    padding: 10px 20px;
-    background-color: #f2f2f2;
+  font-size: 24px;
+  padding: 10px 20px;
+  background-color: #f2f2f2;
 `;
 
 const App = () => {
-    const [tasks, setTasks] = useState([
-        { text: 'aaaaaaaa', isChecked: false },
-        { text: 'bbbbbbbb', isChecked: false },
-        { text: 'cccccccc', isChecked: false },
-    ]);
+  const [tasks, setTasks] = useState([
+    { text: "study react", isChecked: false },
+    { text: "setting css framework", isChecked: false },
+    { text: "modify react portfolio", isChecked: false },
+  ]);
 
-    const handleAddTask = value => {
-        if (value !== '') {
-            setTasks([
-                { text: value, isChecked: false, readOnly: true },
-                ...tasks,
-            ]);
-        }
-    };
+  const handleAddTask = (value) => {
+    if (value !== "") {
+      setTasks([{ text: value, isChecked: false, readOnly: true }, ...tasks]);
+    }
+  };
 
-    const handleDeleteTask = idx => {
-        const result = tasks.filter((task, index) => index !== idx);
-        setTasks(result);
-    };
+  const handleDeleteTask = (idx) => {
+    const result = tasks.filter((task, index) => index !== idx);
+    setTasks(result);
+  };
 
-    const handleModifyTask = idx => {
-        const thisTask = document.querySelector(`#task${idx}`);
-        const thisTaskTitle = thisTask.querySelector('.task').value;
+  const handleModifyTask = (idx) => {
+    const thisTask = document.querySelector(`#task${idx}`);
+    const thisTaskInput = thisTask.querySelector(".task__modify");
+    const buttonModify = thisTask.querySelector(".button__modify");
+    const thisTaskTitle = thisTask.querySelector(".task");
 
-        thisTask.querySelector('.task__modify').value = thisTaskTitle;
-        thisTask.querySelector('.task__modify').focus();
-    };
+    thisTaskInput.value = thisTask.querySelector(".task").value;
+    thisTaskInput.style.display = "inline-block";
+    thisTaskInput.focus();
 
-    const handleModifyConfirmTask = (idx, value) => {
-        setTasks(
-            tasks.map((task, index) =>
-                index === idx
-                    ? { text: value, isChecked: task.isChecked }
-                    : task,
-            ),
-        );
-    };
+    buttonModify.style.display = "inline-block";
+    thisTaskTitle.style.display = "none";
+  };
 
-    const handleCheckTask = idx => {
-        setTasks(
-            tasks.map((task, index) =>
-                index === idx ? { ...task, isChecked: !task.isChecked } : task,
-            ),
-        );
-    };
+  const handleModifyConfirmTask = (idx, value) => {
+    const thisTask = document.querySelector(`#task${idx}`);
+    const thisTaskInput = thisTask.querySelector(".task__modify");
+    const buttonModify = thisTask.querySelector(".button__modify");
+    const thisTaskTitle = thisTask.querySelector(".task");
 
-    return (
-        <div className="App">
-            <Header>ToDo List</Header>
-            <CreateTask handleAddTask={handleAddTask} />
-            <Tasks
-                tasks={tasks}
-                handleCheckTask={handleCheckTask}
-                handleDeleteTask={handleDeleteTask}
-                handleModifyTask={handleModifyTask}
-                handleModifyConfirmTask={handleModifyConfirmTask}
-            />
-        </div>
+    thisTaskInput.style.display = "none";
+    buttonModify.style.display = "none";
+    thisTaskTitle.style.display = "inline-block";
+
+    setTasks(
+      tasks.map((task, index) =>
+        index === idx ? { text: value, isChecked: task.isChecked } : task
+      )
     );
+  };
+
+  const handleCheckTask = (idx) => {
+    setTasks(
+      tasks.map((task, index) =>
+        index === idx ? { ...task, isChecked: !task.isChecked } : task
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <Header>ToDo List</Header>
+      <CreateTask handleAddTask={handleAddTask} />
+      <Tasks
+        tasks={tasks}
+        handleCheckTask={handleCheckTask}
+        handleDeleteTask={handleDeleteTask}
+        handleModifyTask={handleModifyTask}
+        handleModifyConfirmTask={handleModifyConfirmTask}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,110 +1,118 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faTrash, faCloud } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTrash, faCloud } from "@fortawesome/free-solid-svg-icons";
 
 const Task = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 5px 20px;
-    border-bottom: 1px solid #f4f4f4;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 20px;
+  border-bottom: 1px solid #f4f4f4;
 `;
 
 const Inputs = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
 const Checkbox = styled.input`
-    width: 16px;
-    height: 16px;
-    border: 1px solid #ccc;
-    font-size: 12px;
-    vertical-align: middle;
+  width: 16px;
+  height: 16px;
+  border: 1px solid #ccc;
+  font-size: 12px;
+  vertical-align: middle;
 `;
 
 const Input = styled.input`
-    width: calc(100% - 27px);
-    padding: 0 10px;
-    border: none;
-    text-decoration: ${props => (props.dataChecked ? 'line-through' : 'none')};
-    box-sizing: border-box;
+  width: calc(100% - 27px);
+  padding: 0 10px;
+  border: none;
+  text-decoration: ${(props) => (props.dataChecked ? "line-through" : "none")};
+  box-sizing: border-box;
+  &.task__modify {
+    display: none;
+  }
 `;
 
 const Buttons = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const Button = styled.button`
-    height: 25px;
-    width: 30px;
-    margin-left: 1%;
-    border: 1px solid #e5e5e5;
-    background-color: #fff;
-    color: #222;
-    text-transform: uppercase;
-    font-size: 9px;
+  height: 25px;
+  width: 30px;
+  margin-left: 1%;
+  border: 1px solid #e5e5e5;
+  background-color: #fff;
+  color: #222;
+  text-transform: uppercase;
+  font-size: 9px;
+  &.button__modify {
+    display: none;
+  }
 `;
 
 const IconPen = <FontAwesomeIcon icon={faPen} />;
 const IconTrash = <FontAwesomeIcon icon={faTrash} />;
 const IconCloud = <FontAwesomeIcon icon={faCloud} />;
 
-const Tasks = props => {
-    const [value, setValue] = useState('');
+const Tasks = (props) => {
+  const [value, setValue] = useState("");
 
-    const handleChange = event => {
-        setValue(event.target.value);
-    };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    const {
-        tasks,
-        handleCheckTask,
-        handleModifyTask,
-        handleDeleteTask,
-        handleModifyConfirmTask,
-    } = props;
+  const {
+    tasks,
+    handleCheckTask,
+    handleModifyTask,
+    handleDeleteTask,
+    handleModifyConfirmTask,
+  } = props;
 
-    return (
-        <>
-            {tasks.map((task, idx) => (
-                <Task key={idx} id={`task${idx}`}>
-                    <Inputs>
-                        <Checkbox
-                            type="checkbox"
-                            checked={task.isChecked}
-                            onChange={() => handleCheckTask(idx)}
-                        />
-                        <Input
-                            type="text"
-                            className="task"
-                            value={task.text}
-                            dataChecked={task.isChecked}
-                            readOnly
-                        />
-                        <Input
-                            type="text"
-                            className="task__modify"
-                            value={value}
-                            onChange={handleChange}
-                        />
-                    </Inputs>
-                    <Buttons>
-                        <Button
-                            onClick={() => handleModifyConfirmTask(idx, value)}
-                        >
-                            {IconCloud}
-                        </Button>
-                        <Button onClick={() => handleModifyTask(idx)}>
-                            {IconPen}
-                        </Button>
-                        <Button onClick={() => handleDeleteTask(idx)}>
-                            {IconTrash}
-                        </Button>
-                    </Buttons>
-                </Task>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {tasks.map((task, idx) => (
+        <Task key={idx} id={`task${idx}`}>
+          <Inputs>
+            <Checkbox
+              type="checkbox"
+              checked={task.isChecked}
+              onChange={() => handleCheckTask(idx)}
+            />
+            <Input
+              type="text"
+              className="task"
+              value={task.text}
+              dataChecked={task.isChecked}
+              readOnly
+            />
+            <Input
+              type="text"
+              className="task__modify"
+              value={value}
+              onChange={handleChange}
+            />
+          </Inputs>
+          <Buttons>
+            <Button
+              type="button"
+              onClick={() => handleModifyConfirmTask(idx, value)}
+              className="button__modify"
+            >
+              {IconCloud}
+            </Button>
+            <Button type="button" onClick={() => handleModifyTask(idx)}>
+              {IconPen}
+            </Button>
+            <Button type="button" onClick={() => handleDeleteTask(idx)}>
+              {IconTrash}
+            </Button>
+          </Buttons>
+        </Task>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
